bcb0570032c67765daba02f47d6aca39be786b6c
rliechti <rliechti@adobe.com>
2023-10-24 10:06:37 +0200

    linting

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,7 +4,6 @@ import { loadScript, sampleRUM } from './lib-franklin.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
-
 const map = document.querySelector('#locator-map');
 if (map) {
   loadScript('/blocks/locator/locator-init.js', { defer: true });
