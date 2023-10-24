7f6a139b0d836d626287f39cff18539688f4c205
rliechti <rliechti@adobe.com>
2023-10-24 11:58:41 +0200

    add party packs and spirits

--- blocks/table-of-products/table-of-products.js
+++ blocks/table-of-products/table-of-products.js
@@ -5,8 +5,6 @@ export default async function decorate(block) {
   if (block.querySelector('p') == null) {
     const indexedProducts = await getSortedProductsIndex();
     block.querySelectorAll('div').forEach((e) => e.remove());
-
-    // add all products as from index
     indexedProducts.forEach((e) => {
       const entry = document.createElement('div');
       entry.innerHTML = `<a href="${e.path}" title="${e.title}"><h3>${e.title}</h3></a><p class="button-container"><a href="${e.path}" title="${e.title}" class="button secondary">More</a></p>`;
@@ -16,7 +14,7 @@ export default async function decorate(block) {
 
     // add party packs at the end
     const partypacks = document.createElement('div');
-    partypacks.innerHTML = '<a href="/party-packs" title="99 party packs"><h3>99 Party Packs</h3></a><p class="button-container"><a href="/party-packs" title="99 spirits collection" class="button secondary">More</a></p>';
+    partypacks.innerHTML = '<a href="/party-packs" title="99 party packs"><h3>99 Party Packs</h3></a><p class="button-container"><a href="/party-packs" title="99 party packs" class="button secondary">More</a></p>';
     partypacks.querySelector('a').prepend(createOptimizedPicture('/media/media_14dee3756c049dd3ca4ccb186e085f0f17ac2a7e7.png', '99 party packs', false, [{ width: '750' }]));
     block.append(partypacks);
 
